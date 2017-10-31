d143f9a9a361ab6b55be84b7709f8ef572d44701
Sam <basking2@users.noreply.github.com>
2017-10-31 08:12:40 -0500

    Rename pykb to kbsite.

--- kbsite/wsgi.py
+++ kbsite/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for pykb project.
+WSGI config for kbsite project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pykb.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kbsite.settings")
 
 application = get_wsgi_application()
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "pykb.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kbsite.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError:
