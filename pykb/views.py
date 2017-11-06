2eed963a5591a575c8f6b32aa8bf0b19ffc231b4
Sam <sam@i5.samnet>
2017-11-05 22:27:57 -0600

    Secure a URL.

--- pykb/views.py
+++ pykb/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 
+from oauth2_provider.views.generic import ProtectedResourceView
 from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hullo, world.")
 
+class ApiEndpoint(ProtectedResourceView):
+    def get(request):
+        return HttpResponse("Hullo, world.")
