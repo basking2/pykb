40383ed6b484ba2eaedd270aa750e39da7c98c1e
Sam <basking2@users.noreply.github.com>
2017-11-09 07:27:20 -0600

    JWT validated and incorporated. Next steps are session management.

--- pykb/views.py
+++ pykb/views.py
@@ -9,8 +9,11 @@ from django.template.loader import get_template
 from django.utils.http import urlencode
 import json
 import requests
+from jose import jwt
 
 client_json = json.loads(open('client_secret.json').read())
+
+anti_forgery_token = "some random value - fix me."
 
 def index(request):
     t = get_template('index.html')
@@ -21,17 +24,22 @@ def index(request):
         'client_id': client_id,
         'access_type': 'offline',
         'include_granted_scopes': 'true',
-        'state': 'state_parameter_passthrough_value',
+        'state': anti_forgery_token,
         'redirect_uri': 'http://localhost:8000/oauth2',
         'response_type': 'code',
         })
 
     gauth = auth_uri + "?" + q
 
+    login_val = str(request.session['oauth_login_token']) + \
+    "\n\n" + \
+     str(request.session['oauth_id_token'])
+
     h = t.render({
         "var": "val",
         "auth": request.user.is_authenticated(),
         "gauth": gauth,
+        'login': login_val,
     })
 
     return HttpResponse(h)
@@ -39,34 +47,59 @@ def index(request):
 def oauth2(request):
     '''
     Handle OAuth2 responses.
+
+    Specifically, after the initial concent page this goes to the grant, then the access token.
     '''
-    # [08/Nov/2017 06:45:24] "GET /oauth2?state=state_parameter_passthrough_value&code=4/.AACprXXVO83VW4M94kxFcJH2WeXa8oKORrFscML2x072Ibz6lRpap6F_w4_WRhHcOJVemJyNYSO2oo-JWTFSbHM&scope=https://www.googleapis.com/auth/userinfo.profile+https://www.googleapis.com/auth/userinfo.email+https://www.googleapis.com/auth/plus.me&authuser=0&session_state=518c501ce49002a28c88bac3d317f619f5ae7e8c..eed6&prompt=consent HTTP/1.1" 404 2560
     s = "ok\n"
     s += str(request.session)+"\n"
     for k in request.GET:
         s += k +" = "+request.GET[k]+"\n"
 
+    # Check that this request was not forged in some way.
+    if anti_forgery_token != request.GET['state']:
+        raise Exception("State did not match!")
+
+    # Fetch the scope we would like to get a token for.
     scope = ''
     for k in request.GET['scope'].split(' '):
         if (k.endswith('plus.me')):
             scope = k
 
-    client_id = client_json['web']['client_id']
-    client_secret = client_json['web']['client_secret']
-
-    token_uri = client_json['web']['token_uri']
-
     options = {
-        'client_id': client_id,
-        'client_secret': client_secret,
+        'client_id': client_json['web']['client_id'],
+        'client_secret': client_json['web']['client_secret'],
         'grant_type': 'authorization_code',
         'code': request.GET['code'],
         'redirect_uri': 'http://localhost:8000/oauth2',
     }
 
-    resp = requests.post(token_uri, options)
+    resp = requests.post(client_json['web']['token_uri'], options)
 
+    # We got the web token!
     token_json = resp.json()
+
+    # Get the Google Certificates.
+    certs = requests.get(client_json['web']['auth_provider_x509_cert_url']).json()
+
+    # Validate the JWT.
+    id_token = jwt.decode(
+        token_json['id_token'],
+        certs,
+        options = {
+            'verify_aud': False,
+            'verify_iat': False,
+            'verify_exp': False,
+            'verify_nbf': False,
+            'verify_iss': False,
+            'verify_sub': False,
+            'verify_jti': False,
+        },
+        access_token=token_json['access_token']
+    )
+
+
+    request.session['oauth_login_token'] = token_json
+    request.session['oauth_id_token'] = id_token
 
     # access_token
     # expires_in
@@ -75,6 +108,7 @@ def oauth2(request):
 
     s += "\n\nSCOPE "+scope +"\n"
     s += "\n\n"+str(token_json)+"\n\n"
+    s += "\n\n"+str(id_token)+"\n\n"
     return HttpResponse(s, content_type="text/plain", status=200)
 
 @login_required()
