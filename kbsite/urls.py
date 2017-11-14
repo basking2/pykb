2ce4c2ad03dbf657c766ed76e58b97c71c9bfa9e
Sam Baskinger <basking2@yahoo.com>
2017-11-14 16:06:08 -0600

    Adding statick files.

--- kbsite/urls.py
+++ kbsite/urls.py
@@ -14,9 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf.urls import include, url
+from django.conf import settings
+from django.conf.urls.static import static
 #from django.contrib import admin
 
-urlpatterns = [
+urlpatterns = static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + [
     #url(r'^admin/', admin.site.urls),
     url(r'^', include('pykb.urls'))
 ]
