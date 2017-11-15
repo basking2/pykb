8796c4fdd68beaaa9c03aeb7d93645d49648abe2
Sam Baskinger <basking2@yahoo.com>
2017-11-14 21:45:48 -0600

    Adding a near-empty model.

--- pykb/models.py
+++ pykb/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class KbEntry(models.Model):
+    id = models.CharField(primary_key=True, max_length = 1024)
+    text = models.CharField(blank = True, max_length = 1024)
+    expiraton = models.DateTimeField(null=False, blank=False, db_index=True)
