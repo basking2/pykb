7fa80b20234a7db2eb50a52d6bc16bc3ce24ffce
Sam <basking2@yahoo.com>
2017-11-20 16:56:45 -0600

    pykb/oauth2.py: Adding validate section, but no validation is  yet done.

--- pykb/oauth2.py
+++ pykb/oauth2.py
@@ -1,5 +1,7 @@
 import falcon
 
+import json
+
 class OAuth2:
     def __init__(self, **kwargs):
         if 'settings' in kwargs:
@@ -7,5 +9,16 @@ class OAuth2:
 
     def on_get(self, req, resp, action):
         resp.status = falcon.HTTP_200
-        resp.content_type = 'text/plain'
-        resp.body = ("Lorem " "Ipsum. " + str(action))
+        # resp.content_type = 'text/plain'
+
+        if action == 'validate':
+            resp.body = '{"V": "OK"}'
+
+        else:
+            # By default, return the client parameters to make an auth call.
+            resp.body = json.dumps({
+                'client_id': self.settings.client_secret['web']['client_id'],
+                'project_id': self.settings.client_secret['web']['project_id'],
+                'auth_uri': self.settings.client_secret['web']['auth_uri'],
+                'token_uri': self.settings.client_secret['web']['token_uri'],
+            })
