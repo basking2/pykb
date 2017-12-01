ee6d43714c86118ddd81be5fb0d66cc7419e6919
Sam Baskinger <basking2@yahoo.com>
2017-12-01 14:44:24 -0600

    Adding SQLAlchemy.

--- pykb/__init__.py
+++ pykb/__init__.py
@@ -1,5 +1,7 @@
 import falcon
 
+import pykbdb
+
 from pykb.settings import Settings
 from pykb.pykb import PyKb
 # from pykb.oauth2 import OAuth2
@@ -8,6 +10,8 @@ from pykb.oauth2 import OAuth2
 app = falcon.API()
 
 settings = Settings()
+
+print(pykbdb.engine)
 
 app.add_route('/pykb', PyKb(settings = settings))
 app.add_route('/oauth2/{action}', OAuth2(settings = settings))
---
+++ pykbdb/__init__.py
@@ -0,0 +1,35 @@
+# The database ORM stuff for pykb.
+import sqlalchemy
+
+print("SQLALCHEMY VERSION "+sqlalchemy.__version__)
+
+from sqlalchemy import create_engine
+from sqlalchemy import Column
+from sqlalchemy import Integer
+from sqlalchemy import String
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+Base = declarative_base()
+
+# Make a user object.
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+# Create an engine everyone can use.
+engine = create_engine('sqlite:///db.sqlite3', echo=True)
+
+# Migrate the database.
+Base.metadata.create_all(engine)
+
+# How do we make sessions?
+Session = sessionmaker(bind=engine)
+
+session = Session()
+session.add(User(name='hi'))
+print(session.query(User).filter_by(name='ed').first())
+session.close()
