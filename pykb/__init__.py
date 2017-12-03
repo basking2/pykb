2cba859af752f7af5da70f4997b6ebec4402b9be
Sam <basking2@yahoo.com>
2017-12-03 07:43:15 -0600

    Adding a blobs endpoint that does nothing now, but will eventually be secured.

--- pykb/__init__.py
+++ pykb/__init__.py
@@ -6,6 +6,7 @@ from pykb.settings import Settings
 from pykb.pykb import PyKb
 # from pykb.oauth2 import OAuth2
 from pykb.oauth2 import OAuth2
+from pykb.blobs import Blobs
 
 app = falcon.API()
 
@@ -15,3 +16,4 @@ print(pykbdb.engine)
 
 app.add_route('/pykb', PyKb(settings = settings))
 app.add_route('/oauth2/{action}', OAuth2(settings = settings))
+app.add_route('/blobs', Blobs(settings = settings))
---
+++ pykb/blobs.py
@@ -0,0 +1,14 @@
+# How to read and write blobs.
+
+import falcon
+import json
+
+class Blobs():
+    def __init__(self, **kwargs):
+        if 'settings' in kwargs:
+            self.settings = kwargs['settings']
+
+    def on_get(self, req, resp):
+        resp.status = falcon.HTTP_200
+        resp.content_type = 'application/json'
+        resp.body = json.dumps({ "text": "OK" })
