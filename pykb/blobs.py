18030ee444847e22831c7b6c356007e6d1ff3221
Sam Baskinger <basking2@yahoo.com>
2017-12-04 14:56:43 -0600

    Basic session management.

--- pykb/blobs.py
+++ pykb/blobs.py
@@ -2,17 +2,40 @@
 
 import falcon
 import json
+import pykbdb
+import re
+import datetime
 
 class Blobs():
     def __init__(self, **kwargs):
         if 'settings' in kwargs:
             self.settings = kwargs['settings']
 
-    def on_get(self, req, resp):
+    def on_get(self, req, resp, **kwargs):
         print("IN GET BLOBS")
-        if 'Authorization' in req.headers:
-            print("AUTHRIZATION: "+req.headers['Authorization'])
 
+        if 'id' in kwargs:
+            id = kwargs['id']
+        else:
+            id = None
+
+        authorization = req.get_header('Authorization', False, '')
+        m = re.compile('^\s*bearer\s+(.*)$', re.IGNORECASE).\
+            match(authorization)
+        if m:
+            session = pykbdb.Session()
+            websessions = session.query(pykbdb.WebSession).filter(pykbdb.WebSession.authorization == m[1])
+            if websessions.count() > 0:
+                now = datetime.datetime.now()
+                if websessions[0].expiration < now:
+                    session.query(pykbdb.WebSession).filter(pykbdb.WebSession.expiration < now).delete()
+                    session.commit()
+                    print("GOT EXPIRED WS")
+                else:
+                    print("GOT WS")
+            else:
+                print("GOT NO WS")
+            session.close()
 
         resp.status = falcon.HTTP_200
         resp.content_type = 'application/json'
--- pykb/settings.py
+++ pykb/settings.py
@@ -1,5 +1,8 @@
 import json
 
+import datetime
+
 class Settings:
+    session_expiration = datetime.timedelta(seconds = 60 * 60)
     with open('client_secret.json') as io:
         client_secret = json.load(io)
